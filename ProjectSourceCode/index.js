d2fa4ccedc0b02df8eac4c045f0920cd7cbb59b0
Ivan Bury <ivanpbury@gmail.com>
2024-04-03 14:47:07 -0600

    removing incomplete drive api code

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -150,12 +150,6 @@ app.get('/welcome', (req, res) => {
     res.json({status: 'success', message: 'Welcome!'});
 });
 
-const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
-
 // *****************************************************
 // <!-- Section 5 : Start Server-->
 // *****************************************************
