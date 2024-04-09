7f580c68d36373cd145a6a536b56e58fd61226f4
Ivan Bury <ivanpbury@gmail.com>
2024-04-08 22:51:02 -0600

    added sql table for tracking user messages

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -2,3 +2,13 @@ CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    message TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES users(username),
+    FOREIGN KEY (receiver_id) REFERENCES users(username)
+);
