9759ae2dd9e1423ccdda09e5ecd8d96ada03e7bd
aiza8235 <aiza8235@colorado.edu>
2024-04-10 12:21:37 -0600

    Started friendsList.hbs and friends data table

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -7,3 +7,13 @@ CREATE TABLE IF NOT EXISTS files (
     filename VARCHAR(50) NOT NULL,
     data TEXT
 );
+
+CREATE TABLE IF NOT EXISTS friends (
+    friend_id SERIAL PRIMARY KEY,
+    user_username_1 VARCHAR(50) NOT NULL,
+    /*ensure usernames exist in table*/
+    CONSTRAINT fk_user_username_1 FOREIGN KEY (user_username_1) REFERENCES users(username),
+    CONSTRAINT fk_user_username_2 FOREIGN KEY (user_username_2) REFERENCES users(username),
+    /*so user cannot friend themseves */
+    CONSTRAINT check_user_username CHECK (user_username_1 <> user_username_2)
+);
